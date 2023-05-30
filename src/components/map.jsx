30aed3c93bad149bda42955cf489f40dfadea7e9
cjtakhar <cjtakhar@gmail.com>
2023-05-29 21:52:07 -0700

    local storage

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSunrise } from 'react-icons/fi';
 import { BsLightbulb } from 'react-icons/bs';
 import { GrPowerCycle } from 'react-icons/gr';
@@ -8,8 +8,8 @@ import { BsSignpostSplit } from 'react-icons/bs';
 import { CgBee } from 'react-icons/cg';
 import { BiHappyAlt } from 'react-icons/bi';
 import { MdVerticalAlignCenter } from 'react-icons/md';
-import {MdOutlineSportsKabaddi} from 'react-icons/md';
-import {GiBackPain} from 'react-icons/gi';
+import { MdOutlineSportsKabaddi } from 'react-icons/md';
+import { GiBackPain } from 'react-icons/gi';
 import { GiNightSky } from 'react-icons/gi';
 import { GiThreePointedShuriken } from 'react-icons/gi';
 import { GiRainbowStar } from 'react-icons/gi';
@@ -18,105 +18,115 @@ import './styles/map.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Map = () => {
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      title: 'Opening Image',
-      icon: <FiSunrise />,
-      information: ''
-    },
-    {
-      id: 2,
-      title: 'Theme Stated',
-      icon: <BsLightbulb />,
-      information: ''
-    },
-    {
-      id: 3,
-      title: 'Set-Up',
-      icon: <GrPowerCycle />,
-      information: ''
-    },
-    {
-      id: 4,
-      title: 'Catalyst',
-      icon: <GiMatchHead />,
-      information: ''
-    },
-    {
-      id: 5,
-      title: 'Debate',
-      icon: <GrChatOption />,
-      information: ''
-    },
-    {
-      id: 6,
-      title: 'Break Into Two',
-      icon: <BsSignpostSplit />,
-      information: ''
-    },
-    {
-      id: 7,
-      title: 'B Story',
-      icon: <CgBee />,
-      information: ''
-    },
-    {
-      id: 8,
-      title: 'Fun & Games',
-      icon: <BiHappyAlt />,
-      information: ''
-    },
-    {
-      id: 9,
-      title: 'Midpoint',
-      icon: <MdVerticalAlignCenter />,
-      information: ''
-    },
-    {
-      id: 10,
-      title: 'Bad Guys Close In',
-      icon: <MdOutlineSportsKabaddi />,
-      information: ''
-    },
-    {
-      id: 11,
-      title: 'All Is Lost',
-      icon: <GiBackPain />,
-      information: ''
-    },
-    {
-      id: 12,
-      title: 'Dark Night of the Soul',
-      icon: <GiNightSky />,
-      information: ''
-    },
-    {
-      id: 13,
-      title: 'Break Into Three',
-      icon: <GiThreePointedShuriken />,
-      information: ''
-    },
-    {
-      id: 14,
-      title: 'Finale',
-      icon: <GiRainbowStar />,
-      information: ''
-    },
-    {
-      id: 15,
-      title: 'Final Image',
-      icon: <FiSunset />,
-      information: ''
-    },
-  ]);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const savedInformation = localStorage.getItem('cardInformation');
+    if (savedInformation) {
+      setCards(JSON.parse(savedInformation));
+    } else {
+      setCards([
+        {
+          id: 1,
+          title: 'Opening Image',
+          icon: <FiSunrise />,
+          information: ''
+        },
+        {
+          id: 2,
+          title: 'Theme Stated',
+          icon: <BsLightbulb />,
+          information: ''
+        },
+        {
+          id: 3,
+          title: 'Set-Up',
+          icon: <GrPowerCycle />,
+          information: ''
+        },
+        {
+          id: 4,
+          title: 'Catalyst',
+          icon: <GiMatchHead />,
+          information: ''
+        },
+        {
+          id: 5,
+          title: 'Debate',
+          icon: <GrChatOption />,
+          information: ''
+        },
+        {
+          id: 6,
+          title: 'Break Into Two',
+          icon: <BsSignpostSplit />,
+          information: ''
+        },
+        {
+          id: 7,
+          title: 'B Story',
+          icon: <CgBee />,
+          information: ''
+        },
+        {
+          id: 8,
+          title: 'Fun & Games',
+          icon: <BiHappyAlt />,
+          information: ''
+        },
+        {
+          id: 9,
+          title: 'Midpoint',
+          icon: <MdVerticalAlignCenter />,
+          information: ''
+        },
+        {
+          id: 10,
+          title: 'Bad Guys Close In',
+          icon: <MdOutlineSportsKabaddi />,
+          information: ''
+        },
+        {
+          id: 11,
+          title: 'All Is Lost',
+          icon: <GiBackPain />,
+          information: ''
+        },
+        {
+          id: 12,
+          title: 'Dark Night of the Soul',
+          icon: <GiNightSky />,
+          information: ''
+        },
+        {
+          id: 13,
+          title: 'Break Into Three',
+          icon: <GiThreePointedShuriken />,
+          information: ''
+        },
+        {
+          id: 14,
+          title: 'Finale',
+          icon: <GiRainbowStar />,
+          information: ''
+        },
+        {
+          id: 15,
+          title: 'Final Image',
+          icon: <FiSunset />,
+          information: ''
+        },
+      ]);
+    }
+  }, []);
 
   const handleCardClick = (index) => {
-    const updatedCard = prompt('Enter information for the card:');
+    const updatedCard = prompt('Enter information for the card:', cards[index].information);
     if (updatedCard !== null) {
       const newCards = [...cards];
       newCards[index].information = updatedCard;
       setCards(newCards);
+      localStorage.setItem('cardInformation', JSON.stringify(newCards));
     }
   };
 
@@ -131,16 +141,12 @@ const Map = () => {
           >
             <div className="card">
               <div className="card-body">
-              <div className="icon">
+                <div className="icon">
                   {React.cloneElement(card.icon, { size: '3em' })}
                 </div>
                 <h5 className="card-title">{card.title}</h5>
-                
-                {card.information ? (
-                  <p className="card-text">{card.information}</p>
-                ) : (
-                  <p className="card-text"></p>
-                )}
+
+                <p className="card-text">{card.information}</p>
               </div>
             </div>
           </div>
