251a91a2449706973f2d36b3887df406e8f791cc
kt <cjtakhar@gmail.com>
2023-06-09 16:51:05 -0700

    local stroage

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -19,19 +19,24 @@ import './styles/map.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Map = () => {
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      title: 'Opening Image',
-      icon: <FiSunrise />,
-      information: ''
-    },
-    {
-      id: 2,
-      title: 'Theme Stated',
-      icon: <BsLightbulb />,
-      information: ''
-    },
+  const [cards, setCards] = useState(() => {
+    const savedCards = localStorage.getItem('cards');
+    if (savedCards) {
+      return JSON.parse(savedCards);
+    } else {
+      return [
+        {
+          id: 1,
+          title: 'Opening Image',
+          icon: <FiSunrise />,
+          information: ''
+        },
+        {
+          id: 2,
+          title: 'Theme Stated',
+          icon: <BsLightbulb />,
+          information: ''
+        },
     {
       id: 3,
       title: 'Set-Up',
@@ -110,11 +115,14 @@ const Map = () => {
       icon: <FiSunset />,
       information: ''
     },
-  ]);
+  ];
+}
+});
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-  
+useEffect(() => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
     axios.get('http://localhost:5000/api/cards', {
       headers: {
         Authorization: token,
@@ -132,33 +140,36 @@ const Map = () => {
       .catch(error => {
         console.log('Error retrieving cards:', error);
       });
-  }, []);
-  
+  }
+}, []);
 
-  useEffect(() => {
-    const cardInfo = cards.map((card) => card.information || '');
-    localStorage.setItem('cardInfo', JSON.stringify(cardInfo));
-  }, [cards]);  
+useEffect(() => {
+  const cardInfo = cards.map((card) => card.information || '');
+  localStorage.setItem('cardInfo', JSON.stringify(cardInfo));
+}, [cards]);
 
-  const handleCardClick = (index) => {
-    const updatedCard = prompt(
-      `${cards[index].title}:`,
-      cards[index].information
-    );
-  
-    if (updatedCard !== null) {
-      const newCards = [...cards];
-      newCards[index].information = updatedCard !== "" ? updatedCard : "";
-      setCards(newCards);
-  
-      const token = localStorage.getItem('token');
+
+const handleCardClick = (index) => {
+  const updatedCard = prompt(
+    `${cards[index].title}:`,
+    cards[index].information
+  );
+
+  if (updatedCard !== null) {
+    const newCards = [...cards];
+    newCards[index].information = updatedCard !== "" ? updatedCard : "";
+    setCards(newCards);
+    localStorage.setItem('cards', JSON.stringify(newCards));
+
+    const token = localStorage.getItem('token');
+    if (token) {
       const cardData = {
         cards: newCards.map((card) => ({
           id: card.id,
           information: card.information,
         })),
       };
-  
+
       axios.post('http://localhost:5000/api/cards', cardData, {
         headers: {
           Authorization: token,
@@ -171,7 +182,8 @@ const Map = () => {
           console.log('Error saving card:', error);
         });
     }
-  };  
+  }
+}; 
 
   return (
     <div className="container">
