9f218c1028ad6f5ddbd082b5a8f7d7c8141511a7
kt <cjtakhar@gmail.com>
2023-06-09 16:33:29 -0700

    2.0.2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './styles/nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavBar = () => {
   return (
     <Navbar className="navbar" expand="lg">
+      < Link to="/home" className="navbar-brand">
       <Navbar.Brand className="mx-auto">Story Map</Navbar.Brand>
+      </ Link >
     </Navbar>
   );
 };
